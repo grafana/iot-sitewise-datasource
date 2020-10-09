9da9fd30e5b35079e0809e2cb2ccb1639fea81db
mattoa <mattoa@amazon.com>
2020-10-09 10:19:09 -0700

    adding mockgen build target + gitignore.

--- Magefile.go
+++ Magefile.go
@@ -5,7 +5,18 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/sh"
 )
 
 // Default configures the default target.
 var Default = build.BuildAll
+
+var Test = func() error {
+
+	// generate mocks
+	if err := sh.RunV("mockery", "--dir=pkg/sitewise/client/", "--name=Client"); err != nil {
+		return err
+	}
+
+	return build.Test()
+}
