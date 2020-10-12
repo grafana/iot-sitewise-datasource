4c1aebf5cc2433040ea69bcafc142066dd946703
mattoa <mattoa@amazon.com>
2020-10-12 14:06:43 -0700

    Adding framer/resource/meta implementations for Sitewise property value data.

---
+++ pkg/resource/caching_provider.go
@@ -0,0 +1,52 @@
+package resource
+
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/iotsitewise"
+	"github.com/grafana/iot-sitewise-datasource/pkg/sitewise/framer"
+	"github.com/pkg/errors"
+)
+
+// CacheDuration is a constant that defines how long to keep cached elements before they are refreshed
+const CacheDuration = time.Minute * 5
+
+// CacheCleanupInterval is the interval at which the internal cache is cleaned / garbage collected
+const CacheCleanupInterval = time.Minute * 10
+
+// ErrNoValue is returned when a cached value is not available in the local cache
+var ErrNoValue = errors.New("no cached value was found with that key")
+
+type cachingProvider struct {
+	resources *resourceProvider
+	cache     map[string]cachedResult
+}
+
+// cachedResult is a value and a timestamp that defines when the cached value is no longer usable
+type cachedResult struct {
+	Result    *framer.Metadata
+	ExpiresAt time.Time
+}
+
+func newCachedResult(f *framer.Metadata) cachedResult {
+	return cachedResult{
+		ExpiresAt: time.Now().Add(CacheDuration),
+		Result:    f,
+	}
+}
+
+func (cp *cachingProvider) Asset(ctx context.Context, assetId string) (*iotsitewise.DescribeAssetOutput, error) {
+
+	return nil, nil
+}
+
+func (cp *cachingProvider) Property(ctx context.Context, assetId string, propertyId string) (*iotsitewise.DescribeAssetPropertyOutput, error) {
+
+	return nil, nil
+}
+
+func (cp *cachingProvider) AssetModel(ctx context.Context, modelId string) (*iotsitewise.DescribeAssetModelOutput, error) {
+	// TODO: implement if needed
+	return nil, nil
+}
---
+++ pkg/resource/provider.go
@@ -0,0 +1,47 @@
+package resource
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/iotsitewise"
+	"github.com/grafana/iot-sitewise-datasource/pkg/sitewise/client"
+)
+
+type resourceProvider struct {
+	client client.Client
+}
+
+func NewSitewiseResourceProvider(client client.Client) *resourceProvider {
+	return &resourceProvider{
+		client: client,
+	}
+}
+
+func (rp *resourceProvider) Asset(ctx context.Context, assetId string) (*iotsitewise.DescribeAssetOutput, error) {
+
+	resp, err := rp.client.DescribeAssetWithContext(ctx, &iotsitewise.DescribeAssetInput{
+		AssetId: aws.String(assetId),
+	})
+
+	return resp, err
+}
+
+func (rp *resourceProvider) Property(ctx context.Context, assetId string, propertyId string) (*iotsitewise.DescribeAssetPropertyOutput, error) {
+
+	resp, err := rp.client.DescribeAssetPropertyWithContext(ctx, &iotsitewise.DescribeAssetPropertyInput{
+		AssetId:    aws.String(assetId),
+		PropertyId: aws.String(propertyId),
+	})
+
+	return resp, err
+}
+
+func (rp *resourceProvider) AssetModel(ctx context.Context, modelId string) (*iotsitewise.DescribeAssetModelOutput, error) {
+
+	resp, err := rp.client.DescribeAssetModelWithContext(ctx, &iotsitewise.DescribeAssetModelInput{
+		AssetModelId: aws.String(modelId),
+	})
+
+	return resp, err
+}
