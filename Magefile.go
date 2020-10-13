7db739c6f9cb8d9f70d8eaca0c783ff77748301c
mattoa <mattoa@amazon.com>
2020-10-13 15:42:02 -0700

    One last try to fix mockgen.

--- Magefile.go
+++ Magefile.go
@@ -17,7 +17,7 @@ func Default() {
 // this can be changed to look at the directives when more mocks are needed.
 func MockGen() error {
 
-	if err := sh.RunV("go", "get", "github.com/vektra/mockery"); err != nil {
+	if err := sh.RunV("docker", "pull", "vektra/mockery"); err != nil {
 		return err
 	}
 
