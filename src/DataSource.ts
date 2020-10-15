50a44c0f2ee3399e32b4a175fa57b98ff099f209
Ryan McKinley <ryantxu@gmail.com>
2020-10-14 17:01:25 -0700

    add query types

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -1,16 +1,7 @@
-import {
-  DataSourceInstanceSettings,
-  DataQueryResponse,
-  DataFrame,
-  DataQueryRequest,
-  ScopedVars,
-  QueryResultMetaStat,
-} from '@grafana/data';
+import { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
 import { DataSourceWithBackend } from '@grafana/runtime';
-import { Observable } from 'rxjs';
 
-import { SitewiseQuery, SitewiseOptions, SitewiseCustomMeta } from './types';
-import { getRequestLooper, MultiRequestTracker } from 'requestLooper';
+import { SitewiseQuery, SitewiseOptions } from './types';
 
 export class DataSource extends DataSourceWithBackend<SitewiseQuery, SitewiseOptions> {
   // Easy access for QueryEditor
@@ -28,7 +19,7 @@ export class DataSource extends DataSourceWithBackend<SitewiseQuery, SitewiseOpt
    * Do not execute queries that do not exist yet
    */
   filterQuery(query: SitewiseQuery): boolean {
-    return !!query.rawQuery;
+    return !!query.queryType;
   }
 
   getQueryDisplayText(query: SitewiseQuery): string {
@@ -50,152 +41,4 @@ export class DataSource extends DataSourceWithBackend<SitewiseQuery, SitewiseOpt
     // };
     return query;
   }
-
-  query(request: DataQueryRequest<SitewiseQuery>): Observable<DataQueryResponse> {
-    let tracker: SitewiseCustomMeta | undefined = undefined;
-    let queryId: string | undefined = undefined;
-    return getRequestLooper(request, {
-      // Check for a "nextToken" in the response
-      getNextQuery: (rsp: DataQueryResponse) => {
-        if (rsp.data?.length) {
-          const first = rsp.data[0] as DataFrame;
-          const meta = first.meta?.custom as SitewiseCustomMeta;
-          if (meta && meta.nextToken) {
-            queryId = meta.queryId;
-
-            return {
-              refId: first.refId,
-              rawQuery: first.meta?.executedQueryString,
-              nextToken: meta.nextToken,
-            } as SitewiseQuery;
-          }
-        }
-        return undefined;
-      },
-
-      /**
-       * The original request
-       */
-      query: (request: DataQueryRequest<SitewiseQuery>) => {
-        return super.query(request);
-      },
-
-      /**
-       * Process the results
-       */
-      process: (t: MultiRequestTracker, data: DataFrame[], isLast: boolean) => {
-        const meta = data[0]?.meta?.custom as SitewiseCustomMeta;
-        if (!meta) {
-          return data; // NOOP
-        }
-        // Single request
-        meta.fetchStartTime = t.fetchStartTime;
-        meta.fetchEndTime = t.fetchEndTime;
-        meta.fetchTime = t.fetchEndTime! - t.fetchStartTime!;
-
-        if (tracker) {
-          // Additional request
-          if (!tracker.subs?.length) {
-            const { subs, nextToken, queryId, ...rest } = tracker;
-            (rest as any).requestNumber = 1;
-            tracker.subs?.push(rest as SitewiseCustomMeta);
-          }
-          for (const m of tracker.subs!) {
-            delete m.nextToken; // not useful in the
-          }
-          delete (meta as any).queryId;
-          (meta as any).requestNumber = tracker.subs!.length + 1;
-
-          tracker.subs!.push(meta);
-          tracker.fetchEndTime = t.fetchEndTime;
-          tracker.fetchTime = t.fetchEndTime! - tracker.fetchStartTime!;
-          tracker.executionFinishTime = meta.executionFinishTime;
-
-          data[0].meta!.custom = tracker;
-        } else {
-          // First request
-          tracker = {
-            ...t,
-            ...meta,
-            subs: [],
-          } as SitewiseCustomMeta;
-        }
-
-        // Calculate stats
-        if (isLast && tracker.executionStartTime && tracker.executionFinishTime) {
-          delete tracker.nextToken;
-
-          const tsTime = tracker.executionFinishTime - tracker.executionStartTime;
-          if (tsTime > 0) {
-            const stats: QueryResultMetaStat[] = [];
-            if (tracker.subs && tracker.subs.length) {
-              stats.push({
-                displayName: 'HTTP request count',
-                value: tracker.subs.length,
-                unit: 'none',
-              });
-            }
-            stats.push({
-              displayName: 'Execution time (Grafana server ⇆ Timestream)',
-              value: tsTime,
-              unit: 'ms',
-              decimals: 2,
-            });
-            if (tracker.fetchStartTime) {
-              tracker.fetchEndTime = Date.now();
-              const dsTime = tracker.fetchEndTime - tracker.fetchStartTime;
-              tracker.fetchTime = dsTime - tsTime;
-              if (dsTime > tsTime) {
-                stats.push({
-                  displayName: 'Fetch time (Browser ⇆ Grafana server w/o Timestream)',
-                  value: tracker.fetchTime,
-                  unit: 'ms',
-                  decimals: 2,
-                });
-                stats.push({
-                  displayName: 'Fetch overhead',
-                  value: (tracker.fetchTime / dsTime) * 100,
-                  unit: 'percent', // 0 - 100
-                });
-              }
-            }
-            data[0].meta!.stats = stats;
-          }
-        }
-        return data;
-      },
-
-      /**
-       * Callback that gets executed when unsubscribed
-       */
-      onCancel: (tracker: MultiRequestTracker) => {
-        if (queryId) {
-          console.log('Cancelling running timestream query');
-
-          // tracker.killed = true;
-          this.postResource(`cancel`, {
-            queryId,
-          })
-            .then(v => {
-              console.log('Timestream query Canceled:', v);
-            })
-            .catch(err => {
-              err.isHandled = true; // avoid the popup
-              console.log('error killing', err);
-            });
-        }
-      },
-    });
-  }
-}
-
-export function getNextTokenMeta(rsp: DataQueryResponse): SitewiseCustomMeta | undefined {
-  if (rsp.data?.length) {
-    const first = rsp.data[0] as DataFrame;
-    const meta = first.meta?.custom as SitewiseCustomMeta;
-    if (meta && meta.nextToken) {
-      return meta;
-    }
-  }
-  return undefined;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,127 @@
 import { DataQuery } from '@grafana/data';
 import { AwsDataSourceJsonData, AwsDataSourceSecureJsonData } from 'common/types';
 
+// Matches https://github.com/grafana/iot-sitewise-datasource/blob/main/pkg/models/query.go#L3
 export enum QueryType {
-  Builder = 'builder',
-  Samples = 'samples',
-  Raw = 'raw',
+  ListAssetModels = 'ListAssetModels',
+  ListAssets = 'ListAssets',
+  PropertyValue = 'PropertyValue',
+  PropertyValueHistory = 'PropertyValueHistory',
+  PropertyAggregate = 'PropertyAggregate',
 }
 
+export enum SiteWiseQualities {
+  GOOD = 'GOOD',
+  BAD = 'BAD',
+  UNCERTAIN = 'UNCERTAIN',
+}
+
+export enum SiteWiseResolution {
+  Min = '1m',
+  Hour = '1h',
+  Day = '1d',
+  Auto = 'AUTO', // or missing!
+}
+
+export enum AggregateTypes {
+  AVERAGE = 'AVERAGE',
+  COUNT = 'COUNT',
+  MAXIMUM = 'MAXIMUM',
+  MINIMUM = 'MINIMUM',
+  SUM = 'SUM',
+  STANDARD_DEVIATION = 'STANDARD_DEVIATION',
+}
+
+export interface SitewiseQuery extends DataQuery {
+  queryType: QueryType;
+  region?: string; // aws region string
+}
+
+export interface SitewiseQueryWithPages {
+  /**
+   * The next token should never be saved in the JSON model, however some queries
+   * will require multiple pages in order to fulfil the requests
+   */
+  nextToken?: string;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/iot-sitewise/latest/APIReference/API_ListAssetModels.html}
+ */
+export interface ListAssetModelsQuery extends SitewiseQuery, SitewiseQueryWithPages {
+  queryType: QueryType.ListAssetModels;
+}
+
+export function isListAssetModelsQuery(q?: SitewiseQuery): q is ListAssetModelsQuery {
+  return q?.queryType === QueryType.ListAssetModels;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/iot-sitewise/latest/APIReference/API_ListAssetModels.html}
+ */
+export interface ListAssetsQuery extends SitewiseQuery, SitewiseQueryWithPages {
+  queryType: QueryType.ListAssets;
+  assetModelId: string;
+  filter: 'ALL' | 'TOP_LEVEL';
+}
+
+export function isListAssetsQuery(q?: SitewiseQuery): q is ListAssetsQuery {
+  return q?.queryType === QueryType.ListAssets;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/iot-sitewise/latest/APIReference/API_GetAssetPropertyValue.html}
+ * {@link https://github.com/grafana/iot-sitewise-datasource/blob/main/pkg/models/property.go#L15}
+ */
+export interface AssetPropertyValueQuery extends SitewiseQuery {
+  queryType: QueryType.PropertyValue;
+
+  assetId: string;
+  propertyId: string;
+  // NOTE: 'propertyAlias' is not supported, but the UI should be able to show aliases
+}
+
+export function isAssetPropertyValueQuery(q?: SitewiseQuery): q is AssetPropertyValueQuery {
+  return q?.queryType === QueryType.PropertyAggregate;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/iot-sitewise/latest/APIReference/API_GetAssetPropertyValueHistory.html}
+ */
+export interface AssetPropertyValueHistoryQuery extends SitewiseQuery, SitewiseQueryWithPages {
+  queryType: QueryType.PropertyValueHistory;
+
+  assetId: string;
+  propertyId: string;
+  qualities?: SiteWiseQualities[]; // Docs say "Fixed number of 1 item.????" does that mean only one?
+  timeOrdering?: 'ASCENDING' | 'DESCENDING';
+}
+
+export function isAssetPropertyValueHistoryQuery(q?: SitewiseQuery): q is AssetPropertyValueHistoryQuery {
+  return q?.queryType === QueryType.PropertyValueHistory;
+}
+
+/**
+ * {@link https://docs.aws.amazon.com/iot-sitewise/latest/APIReference/API_GetAssetPropertyAggregates.html}
+ */
+export interface AssetPropertyAggregatesQuery extends SitewiseQuery, SitewiseQueryWithPages {
+  queryType: QueryType.PropertyAggregate;
+
+  assetId: string;
+  propertyId: string;
+  resolution?: SiteWiseResolution;
+  aggregateTypes: AggregateTypes[]; // at least one
+  qualities?: SiteWiseQualities[];
+  timeOrdering?: 'ASCENDING' | 'DESCENDING';
+}
+
+export function isAssetPropertyAggregatesQuery(q?: SitewiseQuery): q is AssetPropertyAggregatesQuery {
+  return q?.queryType === QueryType.PropertyAggregate;
+}
+
+/**
+ * Metadata attached to DataFrame results
+ */
 export interface SitewiseCustomMeta {
   queryId: string;
   nextToken?: string;
@@ -23,23 +138,11 @@ export interface SitewiseCustomMeta {
   subs?: SitewiseCustomMeta[];
 }
 
-export interface SitewiseQuery extends DataQuery {
-  // When specified, use this rather than the default for macros
-  database?: string;
-  table?: string;
-  measure?: string;
-
-  // The rendered query
-  rawQuery?: string;
-
-  // Not a real parameter...
-  // nextToken?: string;
-}
-
+/**
+ * Global datasource options
+ */
 export interface SitewiseOptions extends AwsDataSourceJsonData {
-  defaultDatabase?: string;
-  defaultTable?: string;
-  defaultMeasure?: string;
+  // nothing for now
 }
 
 export interface SitewiseSecureJsonData extends AwsDataSourceSecureJsonData {
