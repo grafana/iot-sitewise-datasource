05f55a025d962975ee91a3b183e35734298899d3
Ryan McKinley <ryantxu@gmail.com>
2020-10-21 15:40:31 -0700

    move json to two fields

--- pkg/framer/list_asset_models.go
+++ pkg/framer/list_asset_models.go
@@ -15,14 +15,10 @@ import (
 	"github.com/grafana/iot-sitewise-datasource/pkg/sitewise/resource"
 )
 
-const (
-	assetModelsFrameName = "Asset Models"
-)
-
 type AssetModels iotsitewise.ListAssetModelsOutput
 
-func getAssetModelDescription(asset *iotsitewise.AssetModelSummary) (*string, error) {
-	jb, err := json.Marshal(*asset.Status)
+func getErrorDescription(details *iotsitewise.ErrorDetails) (*string, error) {
+	jb, err := json.Marshal(*details)
 	if err != nil {
 		return nil, err
 	}
@@ -35,30 +31,34 @@ func (a AssetModels) Frames(_ context.Context, _ resource.ResourceProvider) (dat
 	fName := newFieldWithName(fields.Name, data.FieldTypeNullableString, length)
 	fArn := newFieldWithName(fields.Arn, data.FieldTypeNullableString, length)
 	fDescription := newFieldWithName(fields.Description, data.FieldTypeNullableString, length)
-	fId := newFieldWithName(fields.Id, data.FieldTypeNullableString, length)
+	fID := newFieldWithName(fields.Id, data.FieldTypeNullableString, length)
 	fCreationDate := newFieldWithName(fields.CreationDate, data.FieldTypeNullableTime, length)
 	fLastUpdate := newFieldWithName(fields.LastUpdate, data.FieldTypeNullableTime, length)
-	fStatus := newFieldWithName(fields.Status, data.FieldTypeNullableString, length)
+	fStatusError := newFieldWithName("error", data.FieldTypeNullableString, length)
+	fStatusState := newFieldWithName("state", data.FieldTypeNullableString, length)
 
 	for i, asset := range a.AssetModelSummaries {
 
 		fName.Set(i, asset.Name)
 		fArn.Set(i, asset.Arn)
 		fDescription.Set(i, asset.Description)
-		fId.Set(i, asset.Id)
+		fID.Set(i, asset.Id)
 		fCreationDate.Set(i, asset.CreationDate)
 		fLastUpdate.Set(i, asset.LastUpdateDate)
 
-		summary, err := getAssetModelDescription(asset)
-		if err != nil {
-			return nil, err
+		if asset.Status != nil {
+			if asset.Status.Error != nil {
+				val, err := getErrorDescription(asset.Status.Error)
+				if err != nil {
+					fStatusError.Set(i, val)
+				}
+			}
+			fStatusState.Set(i, asset.Status.State)
 		}
-
-		fStatus.Set(i, summary)
 	}
 
-	frame := data.NewFrame(assetModelsFrameName,
-		fName, fDescription, fId, fArn, fStatus, fCreationDate, fLastUpdate,
+	frame := data.NewFrame("",
+		fName, fDescription, fID, fArn, fStatusError, fStatusState, fCreationDate, fLastUpdate,
 	)
 
 	frame.Meta = &data.FrameMeta{
