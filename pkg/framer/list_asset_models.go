907a07632bb1562c9f26570e38a6d766168ebf99
Ryan McKinley <ryantxu@gmail.com>
2020-10-21 15:52:25 -0700

    remove use strings not nullable strings

--- pkg/framer/list_asset_models.go
+++ pkg/framer/list_asset_models.go
@@ -28,23 +28,23 @@ func getErrorDescription(details *iotsitewise.ErrorDetails) (*string, error) {
 func (a AssetModels) Frames(_ context.Context, _ resource.ResourceProvider) (data.Frames, error) {
 	length := len(a.AssetModelSummaries)
 
-	fName := newFieldWithName(fields.Name, data.FieldTypeNullableString, length)
-	fArn := newFieldWithName(fields.Arn, data.FieldTypeNullableString, length)
-	fDescription := newFieldWithName(fields.Description, data.FieldTypeNullableString, length)
-	fID := newFieldWithName(fields.Id, data.FieldTypeNullableString, length)
-	fCreationDate := newFieldWithName(fields.CreationDate, data.FieldTypeNullableTime, length)
-	fLastUpdate := newFieldWithName(fields.LastUpdate, data.FieldTypeNullableTime, length)
+	fName := newFieldWithName(fields.Name, data.FieldTypeString, length)
+	fArn := newFieldWithName(fields.Arn, data.FieldTypeString, length)
+	fDescription := newFieldWithName(fields.Description, data.FieldTypeString, length)
+	fID := newFieldWithName(fields.Id, data.FieldTypeString, length)
+	fCreationDate := newFieldWithName(fields.CreationDate, data.FieldTypeTime, length)
+	fLastUpdate := newFieldWithName(fields.LastUpdate, data.FieldTypeTime, length)
 	fStatusError := newFieldWithName("error", data.FieldTypeNullableString, length)
-	fStatusState := newFieldWithName("state", data.FieldTypeNullableString, length)
+	fStatusState := newFieldWithName("state", data.FieldTypeString, length)
 
 	for i, asset := range a.AssetModelSummaries {
 
-		fName.Set(i, asset.Name)
-		fArn.Set(i, asset.Arn)
-		fDescription.Set(i, asset.Description)
-		fID.Set(i, asset.Id)
-		fCreationDate.Set(i, asset.CreationDate)
-		fLastUpdate.Set(i, asset.LastUpdateDate)
+		fName.Set(i, *asset.Name)
+		fArn.Set(i, *asset.Arn)
+		fDescription.Set(i, *asset.Description)
+		fID.Set(i, *asset.Id)
+		fCreationDate.Set(i, *asset.CreationDate)
+		fLastUpdate.Set(i, *asset.LastUpdateDate)
 
 		if asset.Status != nil {
 			if asset.Status.Error != nil {
@@ -53,7 +53,7 @@ func (a AssetModels) Frames(_ context.Context, _ resource.ResourceProvider) (dat
 					fStatusError.Set(i, val)
 				}
 			}
-			fStatusState.Set(i, asset.Status.State)
+			fStatusState.Set(i, *asset.Status.State)
 		}
 	}
 
