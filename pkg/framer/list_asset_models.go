5bfe0e8e97b0839b194a6928e35dfa0b27a3b1d2
mattoa <mattoa@amazon.com>
2020-10-22 21:59:23 -0700

    fixing error serialization.

--- pkg/framer/list_asset_models.go
+++ pkg/framer/list_asset_models.go
@@ -34,7 +34,7 @@ func (a AssetModels) Frames(_ context.Context, _ resource.ResourceProvider) (dat
 		fLastUpdate.Set(i, *asset.LastUpdateDate)
 
 		if asset.Status.Error != nil {
-			val, err := serialize(asset.Status.Error)
+			val, err := getErrorDescription(asset.Status.Error)
 			if err != nil {
 				fStatusError.Set(i, val)
 			}
