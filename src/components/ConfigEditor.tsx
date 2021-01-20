77332b45170dcddd6157bffe4c1a3abbe0b4ff41
Ryan McKinley <ryantxu@gmail.com>
2021-01-20 10:56:22 -0800

    region change

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,16 +1,13 @@
 import React, { PureComponent } from 'react';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { SitewiseOptions, SitewiseSecureJsonData, SitewiseQuery } from '../types';
+import { SitewiseOptions, SitewiseSecureJsonData } from '../types';
 import ConnectionConfig from '../common/ConnectionConfig';
-import { Alert, TLSAuthSettings } from '@grafana/ui';
+import { Alert } from '@grafana/ui';
 
 export type Props = DataSourcePluginOptionsEditorProps<SitewiseOptions, SitewiseSecureJsonData>;
 
-interface State {
-  schemaState?: Partial<SitewiseQuery>;
-}
 
-export class ConfigEditor extends PureComponent<Props, State> {
+export class ConfigEditor extends PureComponent<Props> {
   constructor(props: Props) {
     super(props);
     this.state = {};
