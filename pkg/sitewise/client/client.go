9e0dd3d03d5864a9a784c01d8afe42b222d03063
Bingpeng Xiang <macrovve@gmail.com>
2021-01-21 18:42:43 -0500

    accept certification when region is edge (#76)

--- pkg/sitewise/client/client.go
+++ pkg/sitewise/client/client.go
@@ -4,9 +4,15 @@ package client
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -84,8 +90,8 @@ func (c *sitewiseClient) GetAssetPropertyAggregatesPageAggregation(ctx context.C
 	}, nil
 }
 
-func GetClient(region string, settings awsds.AWSDatasourceSettings, provider awsds.AmazonSessionProvider) (client SitewiseClient, err error) {
-	sess, err := provider(region, settings)
+func GetClient(region string, settings AWSSiteWiseDataSourceSetting, provider awsds.AmazonSessionProvider) (client SitewiseClient, err error) {
+	sess, err := provider(region, settings.toAWSDatasourceSettings())
 	if err != nil {
 		return nil, err
 	}
@@ -94,7 +100,71 @@ func GetClient(region string, settings awsds.AWSDatasourceSettings, provider aws
 	if settings.Endpoint != "" {
 		swcfg.Endpoint = aws.String(settings.Endpoint)
 	}
+
+	if settings.Region == "Edge" {
+		pool, _ := x509.SystemCertPool()
+		if pool == nil {
+			pool = x509.NewCertPool()
+		}
+
+		if settings.Cert == "" {
+			return nil, errors.New("certificate cannot be null")
+		}
+
+		block, _ := pem.Decode([]byte(settings.Cert))
+		if block == nil || block.Type != "CERTIFICATE" || len(block.Headers) != 0 {
+			return nil, fmt.Errorf("decode certificate failed: %s", settings.Cert)
+		}
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		pool.AddCert(cert)
+
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true, //Not actually skipping, check the cert in VerifyPeerCertificate
+				RootCAs:            pool,
+				VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+					// If this is the first handshake on a connection, process and
+					// (optionally) verify the server's certificates.
+					certs := make([]*x509.Certificate, len(rawCerts))
+					for i, asn1Data := range rawCerts {
+						cert, err := x509.ParseCertificate(asn1Data)
+						if err != nil {
+							return errors.New("tls: failed to parse certificate from server: " + err.Error())
+						}
+						certs[i] = cert
+					}
+
+					opts := x509.VerifyOptions{
+						Roots:         pool,
+						CurrentTime:   time.Now(),
+						DNSName:       "", // <- skip hostname verification
+						Intermediates: x509.NewCertPool(),
+					}
+
+					for i, cert := range certs {
+						if i == 0 {
+							continue
+						}
+						opts.Intermediates.AddCert(cert)
+					}
+					_, err := certs[0].Verify(opts)
+					return err
+				},
+			},
+		}
+		httpClient := &http.Client{Transport: tr}
+
+		swcfg = swcfg.WithHTTPClient(httpClient)
+		swcfg = swcfg.WithDisableEndpointHostPrefix(true)
+
+	}
+
 	c := iotsitewise.New(sess, swcfg)
+
 	c.Handlers.Send.PushFront(func(r *request.Request) {
 		r.HTTPRequest.Header.Set("User-Agent", userAgentString())
 	})
