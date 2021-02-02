95f3d5123b55e4a342429df0e79b6ff53e3e854b
mattoa <mattoa@amazon.com>
2021-02-02 13:43:26 -0800

    [chore] adding boolean test data for compatibility testing

--- pkg/server/test/property_value_history_test.go
+++ pkg/server/test/property_value_history_test.go
@@ -13,7 +13,8 @@ import (
 )
 
 func TestHandlePropertyValueHistory(t *testing.T) {
-	getPropertyValueHistoryHappyCase(t).run(t)
+	//getPropertyValueHistoryHappyCase(t).run(t)
+	getPropertyValueBoolean(t).run(t)
 }
 
 var getPropertyValueHistoryHappyCase testServerScenarioFn = func(t *testing.T) *testScenario {
@@ -48,6 +49,43 @@ var getPropertyValueHistoryHappyCase testServerScenarioFn = func(t *testing.T) *
 			},
 		},
 		goldenFileName: "property-history-values",
+		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
+			return srvr.HandlePropertyValueHistory
+		},
+	}
+}
+
+var getPropertyValueBoolean testServerScenarioFn = func(t *testing.T) *testScenario {
+	mockSw := &mocks.SitewiseClient{}
+
+	propVals := testdata.GetIoTSitewisePropHistoryVals(t, testDataRelativePath("property-history-values-boolean.json"))
+	propDesc := testdata.GetIotSitewiseAssetProp(t, testDataRelativePath("describe-asset-property-is-windy.json"))
+
+	mockSw.On("GetAssetPropertyValueHistoryPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propVals, nil)
+	mockSw.On("DescribeAssetPropertyWithContext", mock.Anything, mock.Anything).Return(&propDesc, nil)
+
+	query := models.AssetPropertyValueQuery{
+		BaseQuery: models.BaseQuery{
+			AwsRegion:  "us-west-2",
+			AssetId:    testdata.DemoTurbineAsset1,
+			PropertyId: testdata.TurbinePropAvgWindSpeed,
+		},
+	}
+
+	return &testScenario{
+		name:   "PropertyValueHistoryResponseHappyCase",
+		mockSw: mockSw,
+		queries: []backend.DataQuery{
+			{
+				QueryType:     models.QueryTypePropertyValueHistory,
+				RefID:         "A",
+				MaxDataPoints: 100,
+				Interval:      1000,
+				TimeRange:     timeRange,
+				JSON:          testdata.SerializeStruct(t, query),
+			},
+		},
+		goldenFileName: "property-history-values-boolean",
 		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
 			return srvr.HandlePropertyValueHistory
 		},
