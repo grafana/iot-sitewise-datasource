08cf7fc0ce9ff3b585333e1764f825c1bf83614a
Atif Ali <atifshoukatali@yahoo.com>
2021-07-22 00:46:06 -0700

    some cleanup

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -1,16 +1,21 @@
 import React, { PureComponent } from 'react';
-import { onUpdateDatasourceResetOption, SelectableValue, updateDatasourcePluginJsonDataOption, updateDatasourcePluginOption, updateDatasourcePluginSecureJsonDataOption } from '@grafana/data';
+import {
+  onUpdateDatasourceResetOption,
+  SelectableValue,
+  updateDatasourcePluginJsonDataOption,
+  updateDatasourcePluginSecureJsonDataOption,
+} from '@grafana/data';
 import { SitewiseOptions, SitewiseSecureJsonData } from '../types';
 import { ConnectionConfig, ConnectionConfigProps } from '@grafana/aws-sdk';
-import { Alert, Button, InlineField, InlineFieldRow, Input, Select } from '@grafana/ui';
+import { Alert, Button, InlineField, InlineFieldRow, Select } from '@grafana/ui';
 import { standardRegions } from '../regions';
 
 export type Props = ConnectionConfigProps<SitewiseOptions, SitewiseSecureJsonData>;
 
 const edgeAuthMethods: Array<SelectableValue<string>> = [
-  {value: 'default', label: 'Default', description: 'default aws auth'},
-  {value: 'linux', label: 'Linux', description: 'default aws auth'},
-  {value: 'ldap', label: 'LDAP', description: 'default aws auth'}
+  { value: 'default', label: 'Default', description: 'Default AWS authentication methods.' },
+  { value: 'linux', label: 'Linux', description: 'Linux-based authentication' },
+  { value: 'ldap', label: 'LDAP', description: 'LDAP-based authentication' },
 ];
 export class ConfigEditor extends PureComponent<Props> {
   constructor(props: Props) {
@@ -22,7 +27,7 @@ export class ConfigEditor extends PureComponent<Props> {
     const { options } = this.props;
     const jsonData = options.jsonData;
     const { defaultRegion, endpoint } = jsonData;
-    const edgeAuthMode = edgeAuthMethods.find(f=>f.value === jsonData.edgeAuthMode) ?? edgeAuthMethods[0];
+    const edgeAuthMode = edgeAuthMethods.find((f) => f.value === jsonData.edgeAuthMode) ?? edgeAuthMethods[0];
 
     return (
       <>
@@ -35,31 +40,32 @@ export class ConfigEditor extends PureComponent<Props> {
                 <Alert title="Edge region requires an explicit endpoint configured above" severity="warning" />
               )}
               <InlineFieldRow>
-                <InlineField label="Auth Mode">
-                  <Select options={edgeAuthMethods} value={edgeAuthMode} onChange={(v) => {
-                        updateDatasourcePluginJsonDataOption(this.props as any, 'edgeAuthType', v.value);
-                      }}/>
+                <InlineField label="Auth Mode"
+                labelWidth={28}
+                tooltip="Specify which authentication method to use.">
+                  <Select
+                    width={30}
+                    options={edgeAuthMethods}
+                    value={edgeAuthMode}
+                    onChange={(v) => {
+                      updateDatasourcePluginJsonDataOption(this.props as any, 'edgeAuthMode' as never, v.value);
+                    }}
+                  />
                 </InlineField>
               </InlineFieldRow>
-              <div className="gf-form-inline">
-                <div className="gf-form gf-form--v-stretch">
-                  <label className="gf-form-label width-14">Certification</label>
-                </div>
-
-                {options.secureJsonFields?.cert ? (
-                  <div className="gf-form">
-                    <div className="max-width-30 gf-form-inline">
-                      <Button
-                        variant="secondary"
-                        type="button"
-                        onClick={onUpdateDatasourceResetOption(this.props as any, 'cert')}
-                      >
-                        Reset
-                      </Button>
-                    </div>
-                  </div>
-                ) : (
-                  <div className="gf-form gf-form--grow">
+              <InlineFieldRow>
+                <InlineField label="Certification"
+                labelWidth={28}
+                tooltip="Certificate for SSL enabled authentication.">
+                  {options.secureJsonFields?.cert ? (
+                    <Button
+                      variant="secondary"
+                      type="reset"
+                      onClick={onUpdateDatasourceResetOption(this.props as any, 'cert')}
+                    >
+                      Reset
+                    </Button>
+                  ) : (
                     <textarea
                       rows={7}
                       className="gf-form-input gf-form-textarea width-30"
@@ -69,9 +75,9 @@ export class ConfigEditor extends PureComponent<Props> {
                       placeholder="Begins with -----BEGIN CERTIFICATE------"
                       required
                     />
-                  </div>
-                )}
-              </div>
+                  )}
+                </InlineField>
+              </InlineFieldRow>
             </div>
           )}
         </div>
