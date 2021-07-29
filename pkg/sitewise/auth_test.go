bc8dd52bd33c5c3cf9abeb9a9a01522882614ac5
Atif Ali <atifshoukatali@yahoo.com>
2021-07-29 12:30:18 -0700

    iserver with TLS auth

--- pkg/sitewise/auth_test.go
+++ pkg/sitewise/auth_test.go
@@ -1,10 +1,21 @@
 package sitewise
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
 	"fmt"
+	"log"
+	"math/big"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/grafana/iot-sitewise-datasource/pkg/models"
 	"github.com/stretchr/testify/require"
@@ -51,13 +62,57 @@ cciS5hf80XzIFqwhzaVS9gmiyM8=
 }
 
 func TestAuthWithServer(t *testing.T) {
-	// TODO: Finish the implementation with cert support
-	t.Skip()
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// generate a new key-pair for the test server TLS
+	rootKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatalf("generating random key: %v", err)
+	}
+
+	// generate a certificate template for the test server TLS
+	rootCertTmpl, err := CertTemplate()
+	if err != nil {
+		log.Fatalf("creating cert template: %v", err)
+	}
+
+	// set the certificate to be used for TLS handshake authentication
+	rootCertTmpl.IsCA = true
+	rootCertTmpl.KeyUsage = x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature
+	rootCertTmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
+	rootCertTmpl.IPAddresses = []net.IP{net.ParseIP("127.0.0.1")} // set the host address here
+
+	// create a self-signed certificate for the test server TLS
+	_, rootCertPEM, err := CreateCert(rootCertTmpl, rootCertTmpl, &rootKey.PublicKey, rootKey)
+	if err != nil {
+		log.Fatalf("error creating cert: %v", err)
+	}
+
+	// PEM encode the private key for TLS server handshake
+	rootKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(rootKey),
+	})
+
+	// Create a TLS certificate using the private key and certificate
+	rootTLSCert, err := tls.X509KeyPair(rootCertPEM, rootKeyPEM)
+	if err != nil {
+		log.Fatalf("invalid key pair: %v", err)
+	}
+
+	// https request handler
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, client")
-	}))
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(handler))
+
+	// Configure the server to present the TLS certificate we created
+	ts.TLS = &tls.Config{
+		Certificates: []tls.Certificate{rootTLSCert},
+	}
+
+	ts.Start()
 	defer ts.Close()
 
+	// test client
 	settings := models.AWSSiteWiseDataSourceSetting{
 		EdgeAuthMode: "linux",
 		EdgeAuthUser: "username",
@@ -65,7 +120,7 @@ func TestAuthWithServer(t *testing.T) {
 	}
 
 	settings.Endpoint = ts.URL
-	settings.Cert = ``
+	settings.Cert = fmt.Sprintf("%s\n", rootCertPEM)
 
 	a := EdgeAuthenticator{Settings: settings}
 	info, err := a.Authenticate()
@@ -73,4 +128,46 @@ func TestAuthWithServer(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, settings.EdgeAuthMode, info.AuthMechanism)
 	require.Equal(t, settings.EdgeAuthUser, info.Username)
+}
+
+// helper function to create a certificate template with a serial number and other required fields
+func CertTemplate() (*x509.Certificate, error) {
+	// generate a random serial number (a real cert authority would have some logic behind this)
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, errors.New("failed to generate serial number: " + err.Error())
+	}
+
+	tmpl := x509.Certificate{
+		SerialNumber:          serialNumber,
+		Subject:               pkix.Name{Organization: []string{"Test Inc."}},
+		SignatureAlgorithm:    x509.SHA256WithRSA,
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(time.Hour), // valid for an hour
+		BasicConstraintsValid: true,
+	}
+	return &tmpl, nil
+}
+
+//helper function to create a certificate from a template and public key plus a parent certificate and private key
+func CreateCert(template, parent *x509.Certificate, pub interface{}, parentPriv interface{}) (
+	*x509.Certificate, []byte, error) {
+
+	certDER, err := x509.CreateCertificate(rand.Reader, template, parent, pub, parentPriv)
+	if err != nil {
+		return nil, nil, errors.New("failed to create a certificate: " + err.Error())
+	}
+
+	// parse the resulting certificate so we can use it again
+	cert, err := x509.ParseCertificate(certDER)
+	if err != nil {
+		return nil, nil, errors.New("failed to parse the certificate: " + err.Error())
+	}
+
+	// PEM encode the certificate (this is a standard TLS encoding)
+	b := pem.Block{Type: "CERTIFICATE", Bytes: certDER}
+	certPEM := pem.EncodeToMemory(&b)
+
+	return cert, certPEM, err
 }
