4821e5a91c3148e98d807be64ce0ce0d2031b235
Kevin Yu <kevinwcyu@users.noreply.github.com>
2023-05-03 13:51:49 -0700

    add test cases

--- pkg/server/test/property_value_history_test.go
+++ pkg/server/test/property_value_history_test.go
@@ -16,6 +16,9 @@ func TestHandlePropertyValueHistory(t *testing.T) {
 	getPropertyValueHistoryHappyCaseTable(t).run(t)
 	getPropertyValueHistoryHappyCaseTimeSeries(t).run(t)
 	getPropertyValueBoolean(t).run(t)
+	getPropertyValueHistoryFromAliasCaseTable(t).run(t)
+	getPropertyValueHistoryFromAliasCaseTimeSeries(t).run(t)
+	getPropertyValueBooleanFromAlias(t).run(t)
 }
 
 var getPropertyValueHistoryHappyCaseTable testServerScenarioFn = func(t *testing.T) *testScenario {
@@ -126,6 +129,123 @@ var getPropertyValueBoolean testServerScenarioFn = func(t *testing.T) *testScena
 			},
 		},
 		goldenFileName: "property-history-values-boolean",
+		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
+			return srvr.HandlePropertyValueHistory
+		},
+	}
+}
+
+var getPropertyValueHistoryFromAliasCaseTable testServerScenarioFn = func(t *testing.T) *testScenario {
+
+	mockSw := &mocks.SitewiseClient{}
+
+	propVals := testdata.GetIoTSitewisePropHistoryVals(t, testDataRelativePath("property-history-values.json"))
+	propDesc := testdata.GetIotSitewiseAssetProp(t, testDataRelativePath("describe-asset-property-avg-wind.json"))
+	propTimeSeries := testdata.GetIoTSitewiseTimeSeries(t, testDataRelativePath("describe-time-series.json"))
+
+	mockSw.On("BatchGetAssetPropertyValueHistoryPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propVals, nil)
+	mockSw.On("DescribeAssetPropertyWithContext", mock.Anything, mock.Anything).Return(&propDesc, nil)
+	mockSw.On("DescribeTimeSeriesWithContext", mock.Anything, mock.Anything).Return(&propTimeSeries, nil)
+
+	query := models.AssetPropertyValueQuery{
+		BaseQuery: models.BaseQuery{
+			AwsRegion:     testdata.AwsRegion,
+			PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		},
+	}
+
+	return &testScenario{
+		name:   "PropertyValueHistoryFromAliasResponseHappyCaseTable",
+		mockSw: mockSw,
+		queries: []backend.DataQuery{
+			{
+				QueryType:     models.QueryTypePropertyValueHistory,
+				RefID:         "A",
+				MaxDataPoints: 100,
+				Interval:      1000,
+				TimeRange:     timeRange,
+				JSON:          testdata.SerializeStruct(t, query),
+			},
+		},
+		goldenFileName: "property-history-values-from-alias-table",
+		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
+			return srvr.HandlePropertyValueHistory
+		},
+	}
+}
+
+var getPropertyValueHistoryFromAliasCaseTimeSeries testServerScenarioFn = func(t *testing.T) *testScenario {
+
+	mockSw := &mocks.SitewiseClient{}
+
+	propVals := testdata.GetIoTSitewisePropHistoryVals(t, testDataRelativePath("property-history-values.json"))
+	propDesc := testdata.GetIotSitewiseAssetProp(t, testDataRelativePath("describe-asset-property-avg-wind.json"))
+	propTimeSeries := testdata.GetIoTSitewiseTimeSeries(t, testDataRelativePath("describe-time-series.json"))
+
+	mockSw.On("BatchGetAssetPropertyValueHistoryPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propVals, nil)
+	mockSw.On("DescribeAssetPropertyWithContext", mock.Anything, mock.Anything).Return(&propDesc, nil)
+	mockSw.On("DescribeTimeSeriesWithContext", mock.Anything, mock.Anything).Return(&propTimeSeries, nil)
+
+	query := models.AssetPropertyValueQuery{
+		BaseQuery: models.BaseQuery{
+			ResponseFormat: "timeseries",
+			AwsRegion:      testdata.AwsRegion,
+			PropertyAlias:  testdata.TurbinePropWindSpeedAlias,
+		},
+	}
+
+	return &testScenario{
+		name:   "PropertyValueHistoryFromAliasResponseHappyCaseTimeSeries",
+		mockSw: mockSw,
+		queries: []backend.DataQuery{
+			{
+				QueryType:     models.QueryTypePropertyValueHistory,
+				RefID:         "A",
+				MaxDataPoints: 100,
+				Interval:      1000,
+				TimeRange:     timeRange,
+				JSON:          testdata.SerializeStruct(t, query),
+			},
+		},
+		goldenFileName: "property-history-values-from-alias-timeseries",
+		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
+			return srvr.HandlePropertyValueHistory
+		},
+	}
+}
+
+var getPropertyValueBooleanFromAlias testServerScenarioFn = func(t *testing.T) *testScenario {
+	mockSw := &mocks.SitewiseClient{}
+
+	propVals := testdata.GetIoTSitewisePropHistoryVals(t, testDataRelativePath("property-history-values-boolean.json"))
+	propDesc := testdata.GetIotSitewiseAssetProp(t, testDataRelativePath("describe-asset-property-is-windy.json"))
+	propTimeSeries := testdata.GetIoTSitewiseTimeSeries(t, testDataRelativePath("describe-time-series.json"))
+
+	mockSw.On("BatchGetAssetPropertyValueHistoryPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propVals, nil)
+	mockSw.On("DescribeAssetPropertyWithContext", mock.Anything, mock.Anything).Return(&propDesc, nil)
+	mockSw.On("DescribeTimeSeriesWithContext", mock.Anything, mock.Anything).Return(&propTimeSeries, nil)
+
+	query := models.AssetPropertyValueQuery{
+		BaseQuery: models.BaseQuery{
+			AwsRegion:     testdata.AwsRegion,
+			PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		},
+	}
+
+	return &testScenario{
+		name:   "PropertyValueHistoryFromAliasResponseBoolean",
+		mockSw: mockSw,
+		queries: []backend.DataQuery{
+			{
+				QueryType:     models.QueryTypePropertyValueHistory,
+				RefID:         "A",
+				MaxDataPoints: 100,
+				Interval:      1000,
+				TimeRange:     timeRange,
+				JSON:          testdata.SerializeStruct(t, query),
+			},
+		},
+		goldenFileName: "property-history-values-from-alias-boolean",
 		handlerFn: func(srvr *server.Server) backend.QueryDataHandlerFunc {
 			return srvr.HandlePropertyValueHistory
 		},
--- pkg/server/test/property_values_for_time_range_test.go
+++ pkg/server/test/property_values_for_time_range_test.go
@@ -29,10 +29,12 @@ func TestHandlePropertyValuesForTimeRange(t *testing.T) {
 		propVals := testdata.GetIoTSitewisePropHistoryVals(t, testDataRelativePath("property-history-values.json"))
 		propAggregates := testdata.GetIoTSitewisePropAggregateVals(t, testDataRelativePath("property-aggregate-values.json"))
 		propDesc := testdata.GetIotSitewiseAssetProp(t, testDataRelativePath("describe-asset-property-avg-wind.json"))
+		propTimeSeries := testdata.GetIoTSitewiseTimeSeries(t, testDataRelativePath("describe-time-series.json"))
 
 		mockSw.On("BatchGetAssetPropertyValueHistoryPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propVals, nil)
 		mockSw.On("BatchGetAssetPropertyAggregatesPageAggregation", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(&propAggregates, nil)
 		mockSw.On("DescribeAssetPropertyWithContext", mock.Anything, mock.Anything).Return(&propDesc, nil)
+		mockSw.On("DescribeTimeSeriesWithContext", mock.Anything, mock.Anything).Return(&propTimeSeries, nil)
 
 		return &testScenario{
 			name: name,
@@ -100,6 +102,41 @@ func TestHandlePropertyValuesForTimeRange(t *testing.T) {
 		AwsRegion:     testdata.AwsRegion,
 		AssetId:       testdata.DemoTurbineAsset1,
 		PropertyId:    testdata.TurbinePropWindSpeed,
+		TimeRange:     backend.TimeRange{From: testdata.FiveMinutes, To: testdata.Now},
+		MaxDataPoints: 299,
+	}, propvals.ResolutionMinute).run(t)
+
+	scenario("raw data for time range from alias", models.BaseQuery{
+		AwsRegion:     testdata.AwsRegion,
+		PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		TimeRange:     backend.TimeRange{From: testdata.FiveMinutes, To: testdata.Now},
+		MaxDataPoints: 720,
+	}, propvals.ResolutionRaw).run(t)
+
+	scenario("1m data for time range from alias", models.BaseQuery{
+		AwsRegion:     testdata.AwsRegion,
+		PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		TimeRange:     backend.TimeRange{From: testdata.TwoHours, To: testdata.Now},
+		MaxDataPoints: 720,
+	}, propvals.ResolutionMinute).run(t)
+
+	scenario("1h data for time range from alias", models.BaseQuery{
+		AwsRegion:     testdata.AwsRegion,
+		PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		TimeRange:     backend.TimeRange{From: testdata.OneDay, To: testdata.Now},
+		MaxDataPoints: 720,
+	}, propvals.ResolutionHour).run(t)
+
+	scenario("1d data for time range from alias", models.BaseQuery{
+		AwsRegion:     testdata.AwsRegion,
+		PropertyAlias: testdata.TurbinePropWindSpeedAlias,
+		TimeRange:     backend.TimeRange{From: testdata.OneMonth, To: testdata.Now},
+		MaxDataPoints: 720,
+	}, propvals.ResolutionDay).run(t)
+
+	scenario("1m data for reduced max data point from alias", models.BaseQuery{
+		AwsRegion:     testdata.AwsRegion,
+		PropertyAlias: testdata.TurbinePropWindSpeedAlias,
 		TimeRange:     backend.TimeRange{From: testdata.FiveMinutes, To: testdata.Now},
 		MaxDataPoints: 299,
 	}, propvals.ResolutionMinute).run(t)
