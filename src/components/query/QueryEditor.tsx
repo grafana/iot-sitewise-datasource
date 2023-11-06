4477513c463bda2e699ea5397c6d91542bd9988c
Ida Štambuk <ida.stambuk@grafana.com>
2023-11-06 13:56:03 +0100

    Config editor

--- src/components/query/QueryEditor.tsx
+++ src/components/query/QueryEditor.tsx
@@ -1,103 +1,301 @@
-import defaults from 'lodash/defaults';
-import React, { PureComponent } from 'react';
-import { QueryEditorProps, SelectableValue } from '@grafana/data';
-import { DataSource } from 'DataSource';
-import { SitewiseQuery, SitewiseOptions, QueryType, ListAssetsQuery } from 'types';
-import { Icon, InlineField, LinkButton, Select } from '@grafana/ui';
-import { QueryTypeInfo, siteWiseQueryTypes, changeQueryType } from 'queryInfo';
-import { standardRegionOptions } from 'regions';
-import { ListAssetsQueryEditor } from './ListAssetsQueryEditor';
-import { PropertyQueryEditor } from './PropertyQueryEditor';
+import React, { ChangeEvent } from 'react';
+import {
+  onUpdateDatasourceJsonDataOption,
+  onUpdateDatasourceJsonDataOptionSelect,
+  onUpdateDatasourceResetOption,
+  SelectableValue,
+  updateDatasourcePluginJsonDataOption,
+  updateDatasourcePluginSecureJsonDataOption,
+} from '@grafana/data';
+import { SitewiseOptions, SitewiseSecureJsonData } from '../types';
+import { ConnectionConfig, ConnectionConfigProps, Divider } from '@grafana/aws-sdk';
+import { Alert, Button, Field, FieldSet, InlineField, InlineFieldRow, Input, Select } from '@grafana/ui';
+import { standardRegions } from '../regions';
+import { config } from '@grafana/runtime';
+import { ConfigSection } from '@grafana/experimental';
 
-type Props = QueryEditorProps<DataSource, SitewiseQuery, SitewiseOptions>;
+export type Props = ConnectionConfigProps<SitewiseOptions, SitewiseSecureJsonData>;
 
-const queryDefaults: Partial<SitewiseQuery> = {
-  maxPageAggregations: 1,
-};
+const edgeAuthMethods: Array<SelectableValue<string>> = [
+  { value: 'default', label: 'Standard', description: 'Use the authentication provider configured above' },
+  { value: 'linux', label: 'Linux', description: 'Linux-based authentication' },
+  { value: 'ldap', label: 'LDAP', description: 'LDAP-based authentication' },
+];
 
-export const firstLabelWith = 20;
-
-export class QueryEditor extends PureComponent<Props> {
-  onQueryTypeChange = (sel: SelectableValue<QueryType>) => {
-    const { onChange, query } = this.props;
-    // hack to use QueryEditor as VariableQueryEditor
-    const onRunQuery = this.props.onRunQuery ?? (() => {});
-    onChange(changeQueryType(query, sel as QueryTypeInfo));
-    onRunQuery();
+export function ConfigEditor(props: Props) {
+  const onUserChange = (event: ChangeEvent<HTMLInputElement>) => {
+    updateDatasourcePluginJsonDataOption(props, 'edgeAuthUser', event.target.value);
   };
+  // @ts-ignore
+  const newFormStylingEnabled = config.featureToggles.awsDatasourcesNewFormStyling;
 
-  onRegionChange = (sel: SelectableValue<string>) => {
-    const { onChange, query, onRunQuery } = this.props;
-    onChange({ ...query, assetId: undefined, propertyId: undefined, region: sel.value });
-    onRunQuery();
-  };
-
-  renderQuery(query: SitewiseQuery) {
-    if (!query.queryType) {
-      return;
-    }
-    switch (query.queryType) {
-      case QueryType.ListAssetModels:
-        return null; // nothing required
-      case QueryType.ListAssets:
-        return <ListAssetsQueryEditor {...this.props} query={query as ListAssetsQuery} />;
-      case QueryType.ListAssociatedAssets:
-      case QueryType.PropertyValue:
-      case QueryType.PropertyInterpolated:
-      case QueryType.PropertyAggregate:
-      case QueryType.PropertyValueHistory:
-        return <PropertyQueryEditor {...this.props} />;
-    }
-    return <div>Missing UI for query type: {query.queryType}</div>;
+  function onPasswordChange(event: ChangeEvent<HTMLInputElement>) {
+    const { options, onOptionsChange } = props;
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        edgeAuthPass: event.target.value,
+      },
+    });
   }
 
-  render() {
-    const { datasource } = this.props;
-    const query = defaults(this.props.query, queryDefaults);
+  function onResetPassword() {
+    const { options, onOptionsChange } = props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        password: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        edgeAuthPass: '',
+      },
+    });
+  }
 
-    const defaultRegion: SelectableValue<string> = {
-      label: `Default`,
-      description: datasource.options?.defaultRegion,
-      value: undefined,
-    };
-    const regions = query.region ? [defaultRegion, ...standardRegionOptions] : standardRegionOptions;
-    const currentQueryType = siteWiseQueryTypes.find((v) => v.value === query.queryType);
-    const queryTooltip = currentQueryType ? (
-      <div>
-        {currentQueryType.description} <br />
-        <LinkButton href={currentQueryType.helpURL} target="_blank">
-          API Docs <Icon name="external-link-alt" />
-        </LinkButton>
-      </div>
-    ) : undefined;
+  function renderEdgeConfig() {
+    const { options } = props;
+    const { jsonData } = options;
+    const { endpoint } = jsonData;
 
-    return (
-      <>
-        <div className="gf-form">
-          <InlineField label="Query type" labelWidth={firstLabelWith} grow={true} tooltip={queryTooltip} interactive>
-            <Select
-              options={siteWiseQueryTypes}
-              value={currentQueryType}
-              onChange={this.onQueryTypeChange}
-              placeholder="Select query type"
-              menuPlacement="bottom"
-            />
-          </InlineField>
-          <InlineField label="Region" labelWidth={14}>
-            <Select
-              width={18}
-              options={regions}
-              value={standardRegionOptions.find((v) => v.value === query.region) || defaultRegion}
-              onChange={this.onRegionChange}
-              backspaceRemovesValue={true}
-              allowCustomValue={true}
-              isClearable={true}
-              menuPlacement="bottom"
-            />
-          </InlineField>
+    const edgeAuthMode = edgeAuthMethods.find((f) => f.value === jsonData.edgeAuthMode) ?? edgeAuthMethods[0];
+    const hasEdgeAuth = edgeAuthMode !== edgeAuthMethods[0];
+    const labelWidth = 28;
+    const regions = standardRegions.map((value) => ({ value, label: value }));
+
+    if (newFormStylingEnabled) {
+      return (
+        <div className="width-30">
+          {hasEdgeAuth && (
+            <ConfigSection title="Connection Details" data-testid="connection-config">
+              <Field
+                label="Endpoint"
+                description="Optionally, specify a custom endpoint for the service"
+                htmlFor="endpoint"
+              >
+                <Input
+                  id="endpoint"
+                  placeholder={endpoint ?? 'https://{service}.{region}.amazonaws.com'}
+                  value={endpoint || ''}
+                  onChange={onUpdateDatasourceJsonDataOption(props, 'endpoint')}
+                />
+              </Field>
+              <Field label="Default Region">
+                <Select
+                  value={regions.find((region) => region.value === options.jsonData.defaultRegion)}
+                  options={regions}
+                  defaultValue={options.jsonData.defaultRegion}
+                  allowCustomValue={true}
+                  onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'defaultRegion')}
+                  formatCreateLabel={(r) => `Use region: ${r}`}
+                />
+              </Field>
+            </ConfigSection>
+          )}
+          {!hasEdgeAuth && (
+            <ConnectionConfig {...props} standardRegions={standardRegions} newFormStylingEnabled={true} />
+          )}
+
+<Divider/>
+          <ConfigSection title="Edge settings" data-testid="edge-settings">
+            {!endpoint && (
+              <Alert title="Edge region requires an explicit endpoint configured above" severity="warning" data-testid="endpoint-warning" />
+            )}
+
+            <Field label="Authentication Mode" htmlFor="edgeAuthMethods">
+              <Select
+              id="edgeAuthMethods"
+              aria-label="Authentication Mode"
+                options={edgeAuthMethods}
+                value={edgeAuthMode}
+                onChange={(v) => {
+                  updateDatasourcePluginJsonDataOption(props, 'edgeAuthMode', v.value);
+                }}
+              />
+            </Field>
+            {hasEdgeAuth && (
+              <>
+                <Field label="Username" description="The username set to local authentication proxy" htmlFor="username">
+                  <Input
+                  id="username"
+                    name="username"
+                    value={jsonData.edgeAuthUser}
+                    autoComplete="off"
+                    className="width-30"
+                    onChange={onUserChange}
+                    required
+                  />
+                </Field>
+                <Field label="Password" description="The password sent to local authentication proxy" htmlFor="password">
+                  <Input
+                  id="password"
+                    type="password"
+                    name="password"
+                    autoComplete="off"
+                    placeholder={options.secureJsonFields?.edgeAuthPass ? 'configured' : ''}
+                    value={options.secureJsonData?.edgeAuthPass ?? ''}
+                    onChange={onPasswordChange}
+                    onReset={onResetPassword}
+                    className="width-30"
+                    required
+                  />
+                </Field>
+              </>
+            )}
+            <Field label="SSL Certificate" description="Certificate for SSL enabled authentication." htmlFor="certificate">
+              {options.secureJsonFields?.cert ? (
+                <Button variant="secondary" type="reset" onClick={onUpdateDatasourceResetOption(props as any, 'cert')} aria-label="Reset certificate input">
+                  Reset
+                </Button>
+              ) : (
+                <textarea
+                id = "certificate"
+                  rows={7}
+                  className="gf-form-input gf-form-textarea width-30"
+                  onChange={(event) => {
+                    updateDatasourcePluginSecureJsonDataOption(props as any, 'cert', event.target.value);
+                  }}
+                  placeholder="Begins with -----BEGIN CERTIFICATE------"
+                  required
+                />
+              )}
+            </Field>
+          </ConfigSection>
         </div>
-        {this.renderQuery(query)}
-      </>
-    );
+      );
+    } else {
+      return (
+        <>
+          {hasEdgeAuth && (
+            <FieldSet label={'Connection Details'} data-testid="connection-config">
+              <InlineField
+                label="Endpoint"
+                labelWidth={28}
+                tooltip="Optionally, specify a custom endpoint for the service"
+              >
+                <Input
+                  className="width-30"
+                  placeholder={endpoint ?? 'https://{service}.{region}.amazonaws.com'}
+                  value={endpoint || ''}
+                  onChange={onUpdateDatasourceJsonDataOption(props, 'endpoint')}
+                />
+              </InlineField>
+              <InlineField
+                label="Default Region"
+                labelWidth={28}
+                tooltip="Specify the region, such as for US West (Oregon) use ` us-west-2 ` as the region."
+              >
+                <Select
+                  className="width-30"
+                  value={regions.find((region) => region.value === options.jsonData.defaultRegion)}
+                  options={regions}
+                  defaultValue={options.jsonData.defaultRegion}
+                  allowCustomValue={true}
+                  onChange={onUpdateDatasourceJsonDataOptionSelect(props, 'defaultRegion')}
+                  formatCreateLabel={(r) => `Use region: ${r}`}
+                />
+              </InlineField>
+            </FieldSet>
+          )}
+          {!hasEdgeAuth && <ConnectionConfig {...props} standardRegions={standardRegions} />}
+
+          <FieldSet label={'Edge settings'} data-testid="edge-connection">
+            {!endpoint && (
+              <Alert title="Edge region requires an explicit endpoint configured above" severity="warning" />
+            )}
+            <InlineFieldRow>
+              <InlineField
+                label="Authentication Mode"
+                labelWidth={labelWidth}
+                tooltip="Specify which authentication method to use."
+              >
+                <Select
+                  className="width-30"
+                  options={edgeAuthMethods}
+                  value={edgeAuthMode}
+                  onChange={(v) => {
+                    updateDatasourcePluginJsonDataOption(props, 'edgeAuthMode', v.value);
+                  }}
+                />
+              </InlineField>
+            </InlineFieldRow>
+            {hasEdgeAuth && (
+              <>
+                <InlineFieldRow>
+                  <InlineField
+                    label="Username"
+                    labelWidth={labelWidth}
+                    tooltip="The username set to local authentication proxy"
+                  >
+                    <Input
+                      name="username"
+                      value={jsonData.edgeAuthUser}
+                      autoComplete="off"
+                      className="width-30"
+                      onChange={onUserChange}
+                      required
+                    />
+                  </InlineField>
+                </InlineFieldRow>
+                <InlineFieldRow>
+                  <InlineField
+                    label="Password"
+                    labelWidth={labelWidth}
+                    tooltip="The password sent to local authentication proxy"
+                  >
+                    <Input
+                      type="password"
+                      name="password"
+                      autoComplete="off"
+                      placeholder={options.secureJsonFields?.edgeAuthPass ? 'configured' : ''}
+                      value={options.secureJsonData?.edgeAuthPass ?? ''}
+                      onChange={onPasswordChange}
+                      onReset={onResetPassword}
+                      className="width-30"
+                      required
+                    />
+                  </InlineField>
+                </InlineFieldRow>
+              </>
+            )}
+            <InlineFieldRow>
+              <InlineField
+                label="SSL Certificate"
+                labelWidth={labelWidth}
+                tooltip="Certificate for SSL enabled authentication."
+              >
+                {options.secureJsonFields?.cert ? (
+                  <Button
+                    variant="secondary"
+                    type="reset"
+                    onClick={onUpdateDatasourceResetOption(props as any, 'cert')}
+                  >
+                    Reset
+                  </Button>
+                ) : (
+                  <textarea
+                    rows={7}
+                    className="gf-form-input gf-form-textarea width-30"
+                    onChange={(event) => {
+                      updateDatasourcePluginSecureJsonDataOption(props as any, 'cert', event.target.value);
+                    }}
+                    placeholder="Begins with -----BEGIN CERTIFICATE------"
+                    required
+                  />
+                )}
+              </InlineField>
+            </InlineFieldRow>
+          </FieldSet>
+        </>
+      );
+    }
   }
+  // Simple
+  if (props.options.jsonData.defaultRegion === 'Edge') {
+    return renderEdgeConfig();
+  }
+  return (
+    <ConnectionConfig {...props} standardRegions={standardRegions} newFormStylingEnabled={newFormStylingEnabled} />
+  );
 }
