a95f41e79d559f50cb8bc292c44c8aaca4fcdc70
Isabella Siu <isabella.siu@grafana.com>
2023-12-01 11:34:56 -0500

    add test

---
+++ pkg/sitewise/client/client_test.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/grafana/grafana-aws-sdk/pkg/awsds"
+	"github.com/grafana/iot-sitewise-datasource/pkg/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient(t *testing.T) {
+	t.Run("Uses region", func(t *testing.T) {
+		var cfg awsds.SessionConfig
+		// deliberately error to avoid the rest of the function
+		mockProvider := func(c awsds.SessionConfig) (*session.Session, error) {
+			cfg = c
+			return nil, fmt.Errorf("break")
+		}
+
+		region := "us-east-1"
+		baseConfig := models.AWSSiteWiseDataSourceSetting{
+			AWSDatasourceSettings: awsds.AWSDatasourceSettings{Region: "us-west-1"},
+		}
+		GetClient(region, baseConfig, mockProvider)
+		assert.Equal(t, cfg.Settings.Region, region)
+	})
+}
