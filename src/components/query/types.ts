21bf1a5d812acf236d2e989a2f5cc00f6ebfa858
Ida Štambuk <ida.stambuk@grafana.com>
2024-08-12 14:05:32 +0200

    Chore: Rename datasource file (#344)

--- src/components/query/types.ts
+++ src/components/query/types.ts
@@ -1,4 +1,4 @@
-import { DataSource } from 'DataSource';
+import { DataSource } from 'SitewiseDataSource';
 import { SitewiseQuery } from 'types';
 
 export interface SitewiseQueryEditorProps<TQuery extends SitewiseQuery = SitewiseQuery> {
