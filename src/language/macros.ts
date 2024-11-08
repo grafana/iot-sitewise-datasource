5ed8455516cdec16846dc20eecf7049250756db9
Jurgens du Toit <jrgns@jadeit.co.za>
2024-11-08 23:24:20 +0200

    feat: Auto completion for raw code editor

---
+++ src/language/macros.ts
@@ -0,0 +1,66 @@
+import { MacroType } from '@grafana/experimental';
+
+const COLUMN = 'column',
+  TIMESTAMP_FORMAT = "'yyyy-MM-dd HH:mm:ss'";
+
+export const TABLE_MACRO = '$__table';
+
+export const MACROS = [
+  {
+    id: '*',
+    name: '*',
+    text: '*',
+    args: [],
+    type: MacroType.Column,
+    description: 'Will be replaced by all the fields of the current table',
+  },
+  {
+    id: '$__rawTimeFrom',
+    name: '$__rawTimeFrom',
+    text: '$__rawTimeFrom',
+    args: [],
+    type: MacroType.Filter,
+    description: 'Will return the current starting time of the time range',
+  },
+  {
+    id: '$__rawTimeTo',
+    name: '$__rawTimeTo',
+    text: '$__rawTimeTo',
+    args: [],
+    type: MacroType.Filter,
+    description: 'Will return the current ending time of the time range',
+  },
+  {
+    id: '$__unixEpochFilter()',
+    name: '$__unixEpochFilter()',
+    text: '$__unixEpochFilter',
+    args: [COLUMN],
+    type: MacroType.Filter,
+    description:
+      'Will be replaced by a time range filter using the specified column name with times represented as Unix timestamp. For example, column >= 1624406400 AND column <= 1624410000',
+  },
+  {
+    id: '$__column',
+    name: '$__column',
+    text: '$__column',
+    args: [],
+    type: MacroType.Column,
+    description: 'Will be replaced by the query column.',
+  },
+  {
+    id: TABLE_MACRO,
+    name: TABLE_MACRO,
+    text: TABLE_MACRO,
+    args: [],
+    type: MacroType.Table,
+    description: 'Will be replaced by the query table.',
+  },
+  {
+    id: '$__parseTime',
+    name: '$__parseTime',
+    text: '$__parseTime',
+    args: [COLUMN, TIMESTAMP_FORMAT],
+    type: MacroType.Value,
+    description: 'Will cast a varchar as a timestamp using the provided format.',
+  },
+];
