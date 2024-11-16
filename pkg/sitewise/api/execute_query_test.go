07733f483b53dfc244953efe9a8684efedc4988a
Jurgens du Toit <jrgns@jadeit.co.za>
2024-11-16 12:51:58 +0200

    chore: Macros, Macro tests and Handler Tests

--- pkg/sitewise/api/execute_query_test.go
+++ pkg/sitewise/api/execute_query_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/iotsitewise"
+	"github.com/grafana/grafana-plugin-sdk-go/data/sqlutil"
 	"github.com/grafana/iot-sitewise-datasource/pkg/models"
 	"github.com/grafana/iot-sitewise-datasource/pkg/sitewise/api"
 	"github.com/stretchr/testify/assert"
@@ -14,11 +15,12 @@ import (
 )
 
 type fakeExecuteQueryClient struct {
-	assetId string
+	lastQueryStatement string
 }
 
 func (f *fakeExecuteQueryClient) ExecuteQueryWithContext(ctx aws.Context, input *iotsitewise.ExecuteQueryInput, opts ...request.Option) (*iotsitewise.ExecuteQueryOutput, error) {
-	retVal := iotsitewise.ExecuteQueryOutput{NextToken: aws.String("bar")} // Fixme
+	f.lastQueryStatement = aws.StringValue(input.QueryStatement)
+	retVal := iotsitewise.ExecuteQueryOutput{NextToken: aws.String("bar")}
 	return &retVal, nil
 }
 
@@ -29,6 +31,18 @@ func TestExecuteQuery(t *testing.T) {
 	}
 	framer, err := api.ExecuteQuery(context.Background(), &client, query)
 	require.NoError(t, err)
-	assert.Equal(t, "foo", client.assetId)
 	assert.Equal(t, "bar", *framer.NextToken)
+}
+func TestExecuteQueryReceivesTheGivenSQL(t *testing.T) {
+	client := &fakeExecuteQueryClient{}
+	query := models.ExecuteQuery{
+		BaseQuery: models.BaseQuery{AssetIds: []string{"foo"}},
+		Query: sqlutil.Query{
+			RawSQL: "SELECT * FROM assets",
+		},
+	}
+	framer, err := api.ExecuteQuery(context.Background(), client, query)
+	require.NoError(t, err)
+	assert.Equal(t, "bar", *framer.NextToken)
+	assert.Equal(t, "SELECT * FROM assets", client.lastQueryStatement)
 }
