c5f3342629dc3b07f5abe59d2fb9078b9b310585
Jurgens du Toit <jrgns@jadeit.co.za>
2025-01-01 07:58:39 +0200

    feat: Region selector for raw query editor

--- src/SitewiseQueryEditor.tsx
+++ src/SitewiseQueryEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from 'react';
-import { ConfirmModal, Space } from '@grafana/ui';
-import { QueryEditorProps } from '@grafana/data';
+import { ConfirmModal, Label, Select, Space } from '@grafana/ui';
+import { QueryEditorProps, SelectableValue } from '@grafana/data';
 import { QueryEditorHeader } from '@grafana/aws-sdk';
 import { reportInteraction } from '@grafana/runtime';
 import { EditorRows, QueryEditorMode, QueryEditorModeToggle } from '@grafana/experimental';
@@ -8,6 +8,7 @@ import { SitewiseQuery, SitewiseOptions, QueryType, SqlQuery } from 'types';
 import { DataSource } from 'SitewiseDataSource';
 import { RawQueryEditor } from 'components/query/query-editor-raw/RawQueryEditor';
 import { VisualQueryBuilder } from 'components/query/visual-query-builder/VisualQueryBuilder';
+import { standardRegionOptions } from 'regions';
 
 type Props = QueryEditorProps<DataSource, SitewiseQuery, SitewiseOptions>;
 
@@ -29,6 +30,10 @@ export function SitewiseQueryEditor(props: Props) {
     },
     [onChange, query]
   );
+
+  const onRegionChange = (sel: SelectableValue<string>) => {
+    onChange({ ...query, region: sel.value });
+  };
 
   const onChangeInternal = (query: SitewiseQuery) => {
     onChange(query);
@@ -65,6 +70,25 @@ export function SitewiseQueryEditor(props: Props) {
         {...props}
         enableRunButton
         extraHeaderElementRight={<QueryEditorModeToggle mode={editorMode!} onChange={onEditorModeChange} />}
+        extraHeaderElementLeft={
+          editorMode == QueryEditorMode.Code ? (
+            <div className="gf-form gf-form--grow">
+              <Label className="gf-form-label width-auto">AWS Region</Label>
+              <Select
+                options={standardRegionOptions}
+                value={
+                  standardRegionOptions.find((v) => v.value === query.region) || props.datasource.options.defaultRegion
+                }
+                onChange={onRegionChange}
+                backspaceRemovesValue={true}
+                allowCustomValue={true}
+                isClearable={true}
+                menuPlacement="auto"
+                width={20}
+              />
+            </div>
+          ) : undefined
+        }
       />
       <Space v={0.5} />
       <EditorRows>
