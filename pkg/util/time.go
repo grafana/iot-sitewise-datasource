f94dba276eda120d1d9672d0ef038723f51cfd0c
patelhiralHM <hiral.patel@happiestminds.com>
2025-06-18 22:12:13 +0530

    Changed the time value from timestamp to date in macros - #486

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -12,3 +12,10 @@ func TimeRangeToUnix(tr backend.TimeRange) (from *time.Time, to *time.Time) {
 	to = aws.Time(time.Unix(tr.To.Unix(), 0))
 	return
 }
+
+// GetFormattedTimeRange returns the time.Time values
+// formatted as UTC strings in the "TIMESTAMP 'YYYY-MM-DD HH:MM:SS'" format.
+func GetFormattedTimeRange(time time.Time) string {
+	const sqlFormat = "2006-01-02 15:04:05"
+	return time.UTC().Format(sqlFormat)
+}
